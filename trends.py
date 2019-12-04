d366acca149f76a4946d48eb2b7e99400236beab
prevay <prevay@groupw.com>
2019-12-04 10:04:13 -0500

    adding notebook for                                       working with charts

--- trends.py
+++ trends.py
@@ -6,6 +6,8 @@ import os
 
 def chart(varcodes, measure, labels=None, from_date=2014, to_date=2018, title=None):
 
+    plt.rcParams['figure.figsize'] = [10, 5]
+
     foldernames = ['DS0001', 'DS0002', 'DS0003', 'DS0004',
                    'DS0005', 'DS0006', 'DS0007']
 
@@ -16,12 +18,14 @@ def chart(varcodes, measure, labels=None, from_date=2014, to_date=2018, title=No
     years = range(from_date, to_date + 1)
 
     for i in years:
-        for foldername in foldernames:
-            filename = next(file for file in os.listdir('../Monitoring_the_Future/{0}/{1}/'.format(i, foldername))
-                            if file.endswith('.tsv'))
-            with open('../Monitoring_the_Future/{0}/{1}/{2}'.format(i, foldername, filename)) as f:
-                volume_df = pd.read_csv(f, sep='\t')
-                for code in varcodes:
+        for code in varcodes:
+            total_occurences = 0
+            data_size = 0
+            for foldername in foldernames:
+                filename = next(file for file in os.listdir('../Monitoring_the_Future/{0}/{1}/'.format(i, foldername))
+                                if file.endswith('.tsv'))
+                with open('Monitoring_the_Future/{0}/{1}/{2}'.format(i, foldername, filename)) as f:
+                    volume_df = pd.read_csv(f, sep='\t')
                     if code in list(volume_df.columns):
                         valid_data = volume_df[code][volume_df[code] != -9]
                         if measure[0] == '=':
@@ -32,10 +36,13 @@ def chart(varcodes, measure, labels=None, from_date=2014, to_date=2018, title=No
                             occurences = valid_data[valid_data > int(measure[1])].size
                         else:
                             print('Invalid comparison operator')
-                        trends_dict[code].append(occurences/valid_data.size)
+                        total_occurences += occurences
+                        data_size += valid_data.size
+            trends_dict[code].append(total_occurences/data_size)
 
     for code in varcodes:
         plt.plot(list(years), trends_dict[code])
+    plt.xticks(years)
     plt.legend(labels)
     plt.title(title)
     plt.show()
